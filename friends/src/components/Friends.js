f232fec7046e0b8771ac2427e5ddf64f68b2c111
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-06-09 10:02:45 -0700

    able to fetch friends from API

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends")
+      .then((res) => setFriends(res.data))
+      .catch((err) => console.log(err.message, err.response));
+  }, []);
+  console.log(friends);
   return (
     <div>
       <h1>Friends</h1>
